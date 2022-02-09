65f00f079e9ecca6bf10692d97d8fcad97e406e9
MassimoGuazzini <97602096+MassimoGuazzini@users.noreply.github.com>
2022-02-09 17:26:23 +0100

    Update and rename terrain_type.r to select_samples.r

--- functions/select_samples.r
+++ functions/select_samples.r
@@ -1,3 +1,4 @@
+
 suppressPackageStartupMessages({
   library(optparse)
 })
@@ -5,18 +6,23 @@ suppressPackageStartupMessages({
 option_list = list(
   make_option(c("-A", "--abundance"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/DESeq2/IT_LC_simpl_2018/ppm.txt",
               help="Input directory [default= %default]", metavar="character"),
-  make_option(c("-M", "--metafile"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/metadata/IT_metadata.xlsx", 
+#  Changing --condition and --token allows to select the samples with specific metadata tags. For instance condition = "Country" and token = "Italy" allows to select only the italian samples. 
+make_option(c("-M", "--metafile"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/metadata/IT_metadata.xlsx", 
               help="List of genes close to Vandal elements [default= %default]", metavar="character"),
-  make_option(c("-C", "--condition"), type="character", default="LC_simpl_2018", 
+  make_option(c("-R", "--readfile"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/metadata/readpingas.txt", 
               help="output file name [default= %default]", metavar="character"),
-  make_option(c("-D", "--value"), type="character", default="grassland", 
+   make_option(c("-C", "--condition"), type="character", default="LC_simpl_2018", 
               help="output file name [default= %default]", metavar="character"),
+  make_option(c("-T", "--token"), type="character", default="Woodland", 
+              help="output file name [default= %default]", metavar="character")
+  # make_option(c("-R", "--raw_counts"), type="character", default=NULL,
+              # help="raw (total) read counts for this starting file", metavar="character")
 )
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
-#print("USAGE: $ 01_small_RNA.r -I input dir/ -O summarized output file")
+#print("USATE: $ 01_small_RNA.r -I input dir/ -O summarized output file")
 
 if (is.null(opt$abundance)) {
   stop("WARNING: No abundance specified with '-I' flag.")
@@ -49,19 +55,14 @@ if (is.null(opt$readfile)) {
   #setwd(wd_location)  
   }
 
-  if (is.null(opt$graphdir)) {
-  stop("WARNING: No graphdir specified with '-G' flag.")
-} else {  cat ("graphdir is ", opt$graphdir, "\n")
-  graphdir <- opt$graphdir  
+  if (is.null(opt$token)) {
+  stop("WARNING: No token specified with '-T' flag.")
+} else {  cat ("token is ", opt$token, "\n")
+  token <- opt$token  
   }
  
-   if (is.null(opt$graphdir)) {
-  stop("WARNING: No graphdir specified with '-D' flag.")
-} else {  cat ("graphdir is ", opt$graphdir, "\n")
-  graphdir <- opt$graphdir  
-  } 
   
-    library(data.table)
+      library(data.table)
 	library(ape)
 	library(openxlsx)
    library("stringr")
@@ -70,14 +71,18 @@ if (is.null(opt$readfile)) {
   metadata<-read.xlsx(metafile)
 	countdata<-fread(abundance,data.table=F)
 	metadata$BARCODE_ID<-paste0("L",metadata$BARCODE_ID)
+
 	#Condition based on soil type: LC_simpl_2018
+	#No covariates
+	#We store bigmeta in memory for later re-use
 	bigmeta<-metadata
  	metadata<-metadata[,c("BARCODE_ID",condition)]
-#select the samples with the desidered terrain type
-  metadata = lapply(metadata, subset, metadata$LC_simpl_2018 == "Woodland") 
+    metadata = lapply(metadata, subset, metadata$LC_simpl_2018 == token)
+  browser()  
  	names(metadata)[2]<-"condition"
  	rownames(countdata)<-countdata$Drug_Class
   countdata$Drug_Class = NULL
   #keep only the samples present in metadata
  	countdata<-countdata[,names(countdata)%in%metadata$BARCODE_ID]
-write.table(countdata,"Woodland", quote=F)
+  
+write.table(countdata,file=paste( token, ".txt", sep=""), row.names=F, quote=F)
