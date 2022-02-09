d9a288a409e55109b98acaf293a61f06af9588a4
MassimoGuazzini <97602096+MassimoGuazzini@users.noreply.github.com>
2022-02-09 10:12:09 +0100

    Update DESeq2.r

--- functions/DESeq2.r
+++ functions/DESeq2.r
@@ -5,17 +5,17 @@ suppressPackageStartupMessages({
 })
 
 option_list = list(
-  make_option(c("-A", "--abundance"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/analysis/all/5_stats",
+  make_option(c("-A", "--abundance"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/analysis/all/aro_to_class_tmp.txt",
               help="Input directory [default= %default]", metavar="character"),
-  make_option(c("-M", "--metafile"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/metadata/metadata.xlsx", 
+  make_option(c("-M", "--metafile"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/metadata/IT_metadata.xlsx", 
               help="List of genes close to Vandal elements [default= %default]", metavar="character"),
   make_option(c("-R", "--readfile"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/metadata/readpingas.txt", 
               help="output file name [default= %default]", metavar="character"),
-  make_option(c("-O", "--out"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/DESeq2/LC_simpl_2018_merge/", 
+  make_option(c("-O", "--out"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/DESeq2/IT_LC_simpl_2018/", 
               help="output file name [default= %default]", metavar="character"),
   make_option(c("-C", "--condition"), type="character", default="LC_simpl_2018", 
               help="output file name [default= %default]", metavar="character"),
-  make_option(c("-G", "--graphdir"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/DESeq2/LC_simpl_2018_merge/", 
+  make_option(c("-G", "--graphdir"), type="character", default="/mnt/vol1/projects/LUCAS/JRC_result/DESeq2/IT_LC_simpl_2018/", 
               help="output file name [default= %default]", metavar="character")
   # make_option(c("-R", "--raw_counts"), type="character", default=NULL,
               # help="raw (total) read counts for this starting file", metavar="character")
@@ -77,8 +77,6 @@ runDEseq<-function()
 	library("pheatmap")
 	library("ggplot2")
 	library("ggrepel")
-  library("stringr")
-  library("dplyr")
 	metadata<-read.xlsx(metafile)
 	countdata<-fread(abundance,data.table=F)
 	metadata$BARCODE_ID<-paste0("L",metadata$BARCODE_ID)
@@ -88,13 +86,13 @@ runDEseq<-function()
 	bigmeta<-metadata
 	metadata<-metadata[,c("BARCODE_ID",condition)]
 	names(metadata)[2]<-"condition"
-  countdata$`ARO|Name|Drug_Class|Antibiotic|Resistance_Mechanism` = str_c(countdata$ARO, '|' , countdata$Name, '|' , countdata$Drug_Class, '|' , countdata$Antibiotic, '|' , countdata$Resistance_Mechanism)
-  countdata=countdata %>% relocate(`ARO|Name|Drug_Class|Antibiotic|Resistance_Mechanism`)
-	rownames(countdata)<-countdata$`ARO|Name|Drug_Class|Antibiotic|Resistance_Mechanism`
-	countdata$ARO<-countdata$Name<-countdata$Drug_Class<-countdata$Antibiotic<-countdata$Resistance_Mechanism<-NULL
+	rownames(countdata)<-countdata$Drug_class
+	countdata$Drug_class<-NULL
 	readcount<-fread(readfile,data.table=F)
-	countdata<-countdata[,names(countdata)%in%readcount$V1]
-	metadata<-metadata[metadata$BARCODE_ID%in%readcount$V1,]
+ #keep only the samples present in metadata
+  readcount<-readcount[readcount$V1%in%metadata$BARCODE_ID,]
+	countdata<-countdata[,names(countdata)%in%metadata$BARCODE_ID]
+	metadata<-metadata[metadata$BARCODE_ID%in%metadata$BARCODE_ID,]
 	rownames(metadata)<-metadata$BARCODE_ID
 	#Add (unmapped) read counts to the count table (needed to normalize)
 	#Trick to sort the read counts in the same order of the gene counts based on sample names
@@ -103,8 +101,8 @@ runDEseq<-function()
 	tcount<-sreadcount[,2]
 	totmapped<-apply(countdata,2,sum)
 	unmapped<-tcount-totmapped
-	countdata<-data.frame(rbind(countdata,unmapped),stringsAsFactors=F)
-	rownames(countdata)[nrow(countdata)]<-"Unmapped"
+	unmapped_countdata<-data.frame(rbind(countdata,unmapped),stringsAsFactors=F)
+	rownames(unmapped_countdata)[nrow(unmapped_countdata)]<-"Unmapped"
 	#Check if metadata has NA and in case assign them to Unknown
 	metadata$condition[is.na(metadata$condition)]<-"Unknown"
 	#Strip leading and trailing whitespace from condition
@@ -112,17 +110,20 @@ runDEseq<-function()
 	metadata$condition<-gsub(" ","",metadata$condition)
 	#Check that samples are exactly the same and in the same order between counts and metadata
 	if(sum(rownames(metadata)==names(countdata))!=ncol(countdata)) stop("Names in countdata do not match names in metadata")
-    
 	ddsHTSeq <- DESeqDataSetFromMatrix(countData  = countdata,
                                         colData = metadata,
                                         design= ~ condition)
+ browser()
 	#Remove low abundance genes
     keep <- rowSums(counts(ddsHTSeq)) >= 5 
     ddsHTSeq<-ddsHTSeq[keep,]
 	ddsHTSeq<-DESeq(ddsHTSeq)
-  write.table(counts(ddsHTSeq,normalized=TRUE),paste(outdir,"norm_counts.txt",sep=""),sep="\t",quote=F,col.names=NA)
-  write.table(counts(ddsHTSeq,normalized=FALSE),paste(outdir,"raw_counts.txt",sep=""),sep="\t",quote=F,col.names=NA)
-    ppm<-counts(ddsHTSeq,normalized=FALSE)
+    write.table(counts(ddsHTSeq,normalized=TRUE),paste(outdir,"norm_counts.txt",sep=""),sep="\t",quote=F,col.names=NA)
+    write.table(counts(ddsHTSeq,normalized=FALSE),paste(outdir,"raw_counts.txt",sep=""),sep="\t",quote=F,col.names=NA)
+ unmapped_ddsHTSeq <- DESeqDataSetFromMatrix(countData  = unmapped_countdata,
+                                        colData = metadata,
+                                        design= ~ condition)
+    ppm<-counts(unmapped_ddsHTSeq,normalized=FALSE)
 	ppm<-ppm*1000000
 	ppm<-t(apply(ppm,1,"/",tcount))
 	write.table(ppm,paste(outdir,"ppm.txt",sep=""),sep="\t",quote=F,col.names=NA)
@@ -146,7 +147,8 @@ runDEseq<-function()
 	pdf(paste0(outdir,"VST_PCA.pdf"))
 	#plotPCA(vsd, intgroup=c("condition"))
 	pino<-plotPCA(vsd, intgroup=c("condition"),returnData=T)
-	pippo<-ggplot(pino, aes(x=PC1, y=PC2, color=group),size=3)+geom_point(size=3) +geom_text_repel(aes(label=name),size=4) + coord_fixed() + theme(legend.text=element_text(size=10)) +xlab(myxlab) + ylab(myylab)
+ browser()
+	pippo<-ggplot(pino, aes(x=PC1, y=PC2, color=group),size=3)+geom_point(size=3) +geom_text_repel(aes(label=name),size=4) + coord_fixed() + theme(legend.text=element_text(size=10)) +xlab(myxlab) + ylab(myylab) + scale_color_manual(values=c("Gold3", "Green", "Brown"))
 	print(pippo)
 	dev.off()
 	# pdf("dispersion_fit.pdf")
@@ -156,10 +158,20 @@ runDEseq<-function()
 	#Build heatmap based on the 50 most present genes
 	newcounts<-counts(ddsHTSeq,normalized=TRUE)
 	#We don't want to use unmapped, and we set them to zero
-	newcounts[row.names(newcounts)=="Unmapped",]<-0
-	keepme <- order(rowMeans(newcounts), decreasing=TRUE)[1:50]
+	newcounts=newcounts[rownames(newcounts)!="Unmapped",]
+	keepme <- order(rowMeans(newcounts), decreasing=TRUE)[1:min(nrow(newcounts),50)]
 	pcond<-data.frame(condition=metadata$condition,row.names=rownames(metadata))
-	pheatmap(assay(vsd)[keepme,], cluster_rows=FALSE, annotation_col = pcond, fontsize=4, cellwidth=6, cellheight=4, filename=paste0(graphdir,"50-most-abundant-genes_clust.pdf"))
+	pheatmap(newcounts[keepme,], cluster_rows=FALSE, annotation_col = pcond, fontsize=4, cellwidth=6, cellheight=4, filename=paste0(graphdir,"50-most-abundant-genes_clust.pdf"))
+  somma=rowSums(assay(vsd))
+  pilo=assay(vsd)
+  pilo=pilo[order(somma, decreasing = T),][1:min(nrow(pilo),50),]
+  browser()
+  pcond_color=pcond
+  pcond_color$condition=as.factor(pcond$condition)
+  ann_colors <- list(condition = c(Cropland = "Gold3",Grassland = "Green",Woodland = "Brown"))
+  
+pheatmap(pilo, cluster_rows=FALSE, annotation_col = pcond_color, annotation_colors = ann_colors, fontsize=4, cellwidth=6, cellheight=4, filename=paste0(graphdir,"vsd_50-most-abundant-genes_clust.pdf"))
+
     #dev.off()
 	#Plot distance matrix as heatmap
 	sampleDist <- dist(t(assay(vsd)))
@@ -168,7 +180,7 @@ runDEseq<-function()
 	rownames(sampleDistMatrix) <- colnames(vsd)
 	colnames(sampleDistMatrix) <- colnames(vsd)
 	colors <- colorRampPalette ( rev( brewer.pal(9 , "Blues")) )( 255)
-	pheatmap(sampleDistMatrix, clustering_distance_rows =sampleDist, annotation_col = pcond, annotation_row = pcond, clustering_distance_cols =sampleDist, col=colors, filename=paste0(graphdir,"VST_samples-distances.pdf"))
+	pheatmap(sampleDistMatrix, clustering_distance_rows =sampleDist, annotation_col = pcond_color, annotation_colors = ann_colors, annotation_row = pcond, clustering_distance_cols =sampleDist, col=colors, filename=paste0(graphdir,"VST_samples-distances.pdf"))
 	#dev.off()
 
 	#We are now comparing all vs all
@@ -178,10 +190,12 @@ runDEseq<-function()
     {
  		res <- results(ddsHTSeq,contrast=c("condition",compare[2,aaa],compare[1,aaa]))
         res<-res[order(res$padj),]
+        res=data.frame(class=row.names(res),res)
         resfile<-paste(outdir,condition,"_",compare[2,aaa],"_vs_",compare[1,aaa],".txt",sep="")
-		write.table(res,resfile,sep="\t",quote=F)
+		write.table(res,resfile,sep="\t",quote=F,row.names=F)
 		mysig<-sum(res$padj<=0.05,na.rm=T)
 		cat(compare[2,aaa],"vs",compare[1,aaa],",",mysig,"significant genes out of",nrow(res),"\n")
+   
 	}
 
 }
