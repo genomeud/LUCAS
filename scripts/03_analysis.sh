e5cee8f89325b0f6465886c4cbf4663b06b76edf
MassimoGuazzini <97602096+MassimoGuazzini@users.noreply.github.com>
2022-02-11 16:58:51 +0100

    Update 03_analysis.sh

--- scripts/03_analysis.sh
+++ scripts/03_analysis.sh
@@ -8,8 +8,8 @@ Rscript terrain_type.r --token="woodland"
 Rscript terrain_type.r --token="grassland"
 #Calculate the average ppm among samples of every drug class and join the terrain on a single table
 for file in $(ls *land); do awk '{sum = 0; for (i = 2; i <= NF; i++) sum += $i; sum /= (NF-1); print$0 " "  sum}' $file > outfile; awk '{print$1 " " $NF}' outfile |  awk '{if (NR!=1) {print}}' > ${file}_average.txt; done
-join Cropland_average.txt Woodland_average.txt > tmp
-join tmp Grassland_average.txt > tmp2
+join cropland_average.txt woodland_average.txt > tmp
+join tmp grassland_average.txt > tmp2
 #Calculate the average of every row and select the 10 most abundant drug classes
 awk '{sum = 0; for (i = 2; i <= NF; i++) sum += $i; sum /= (NF-1); print$0 " "  sum}' tmp2 | sort -n -k 4 -r | awk '{print $1 " " $2 " " $3 " " $4} NR==10{exit}' > ppm_average_terrain_type.txt
 sed -i '1 i\Drug_Class Cropland Woodland Grassland' ppm_average_terrain_type.txt
