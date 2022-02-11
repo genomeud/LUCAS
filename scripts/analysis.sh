9776b7a4e79647dc7d3d8a826e5f0cbece14739a
MassimoGuazzini <97602096+MassimoGuazzini@users.noreply.github.com>
2022-02-11 12:11:45 +0100

    Update analysis.sh

--- scripts/analysis.sh
+++ scripts/analysis.sh
@@ -9,8 +9,9 @@ Rscript terrain_type.r --token="grassland"
 #Calculate the average ppm among samples of every drug class and join the terrain on a single table
 for file in $(ls *land); do awk '{sum = 0; for (i = 2; i <= NF; i++) sum += $i; sum /= (NF-1); print$0 " "  sum}' $file > outfile; awk '{print$1 " " $NF}' outfile |  awk '{if (NR!=1) {print}}' > ${file}_average.txt; done
 join Cropland_average.txt Woodland_average.txt > tmp
-join tmp Grassland_average.txt > final
-sed 's/|/ /g' final > ppm_average_terrain_type.txt
+join tmp Grassland_average.txt > tmp2
+#Calculate the average of every row and select the 10 most abundant drug classes
+awk '{sum = 0; for (i = 2; i <= NF; i++) sum += $i; sum /= (NF-1); print$0 " "  sum}' tmp2 | sort -n -k 4 -r | awk '{print $1 " " $2 " " $3 " " $4} NR==10{exit}' > final
 sed -i '1 i\Drug_Class Cropland Woodland Grassland' ppm_average_terrain_type.txt
 #produce stacked bar plot of drug class in italy grouped by country and plot the Log2FoldChange
 Rscript bar_plot.r
