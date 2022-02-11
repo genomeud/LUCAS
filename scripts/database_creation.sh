bd002061c9528f883167ff3f42c37fcb3c064f00
MassimoGuazzini <97602096+MassimoGuazzini@users.noreply.github.com>
2022-02-11 15:29:07 +0100

    Update database_creation.sh

--- scripts/database_creation.sh
+++ scripts/database_creation.sh
@@ -1,11 +1,16 @@
+#The database can be found at https://card.mcmaster.ca/download/0/broadstreet-v3.1.4.tar.bz2
+KRAKENBUILD=/mnt/vol1/projects/SRAome/kraken2/kraken2-build
 DATABASE=/mnt/vol1/projects/LUCAS/test_run/database/homolog_model/nucleotide_fasta_protein_homolog_model.fasta
 DATABASENAME=nucleotide_fasta_protein_homolog_model
+module load aligners/bwa/0.7.13
+#Dustmasker is included in blast+
+module load aligners/blast/2.6.0+
 #kraken, we need to add a dummy "taxid" to every database entry 
 awk -F "|" '{if($1 ~/^>/) {print $1"|"$3"|kraken:taxid|32630"}else{print $0}}' $DATABASE > ${DATABASENAME}_kraken.fasta
 #creation of custom kraken database
-kraken2-build --download-taxonomy --db $DATABASENAME
-kraken2-build --add-to-library ${DATABASE%.*}_kraken.fasta --db $DATABASENAME
-kraken2-build --build --threads 24 --db $DATABASENAME
+$KRAKENBUILD --download-taxonomy --db $DATABASENAME
+$KRAKENBUILD --add-to-library ${DATABASE%.*}_kraken.fasta --db $DATABASENAME
+$KRAKENBUILD --build --threads 24 --db $DATABASENAME
 #bwa index
 bwa index -p $DATABASENAME -a bwtsw $DATABASE
 #dustmasker, the output will be used by samtools to mask low-abundance regions from BAM files.
