cae7d696dc320ad9eaf2c6df216cc7df3d719728
MassimoGuazzini <97602096+MassimoGuazzini@users.noreply.github.com>
2022-03-04 11:43:28 +0100

    Update 03_analysis.sh

--- scripts/03_analysis.sh
+++ scripts/03_analysis.sh
@@ -2,17 +2,53 @@
 Rscript ARO_to_class.r
 #Produce the raw,normalized and ppm counts, plus Deseq2 analysis, PCA and heatmaps of "italy" tagged samples
 Rscript DESeq2.r
-#Group the samples in "italy" based on terrain type
-Rscript terrain_type.r --token="cropland"
-Rscript terrain_type.r --token="woodland"
-Rscript terrain_type.r --token="grassland"
-#Calculate the average ppm among samples of every drug class and join the terrain on a single table
-for file in $(ls *land*); do awk '{sum = 0; for (i = 2; i <= NF; i++) sum += $i; sum /= (NF-1); print$0 " "  sum}' $file > outfile; awk '{print$1 " " $NF}' outfile |  awk '{if (NR!=1) {print}}' > ${file%.*}_average.txt; done
-join cropland_average.txt woodland_average.txt > tmp
-join tmp grassland_average.txt > tmp2
-#Calculate the average of every row and select the 10 most abundant drug classes
-awk '{sum = 0; for (i = 2; i <= NF; i++) sum += $i; sum /= (NF-1); print$0 " "  sum}' tmp2 | sort -n -k 4 -r | awk '{print $1 " " $2 " " $3 " " $4} NR==10{exit}' > ppm_average_terrain_type.txt
-sed -i '1 i\Drug_Class Cropland Woodland Grassland' ppm_average_terrain_type.txt
+#Group the samples based on subclass
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Broadleaved_woodland"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Cereals"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Coniferous_woodland"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Dry_pulses_vegetables_and_flowers"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Fodder_crops"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Grassland_with_sparse_tree_shrub_cover"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Grassland_without_tree_shrub_cover"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Mixed_woodland"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Nonpermanent_industrial_crops"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Other_permanent_crops"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Permanent_crops_fruit_trees"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Root_crops"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Spontaneously_revegetated_surfaces"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Others"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Wetland"
+#calculate the average
+for file in $(ls); do awk '{sum = 0; for (i = 2; i <= NF; i++) sum += $i; sum /= (NF-1); print$0 " "  sum}' $file > outfile; awk '{print$1 " " $NF}' outfile |  awk '{if (NR!=1) {print}}' > average/${file%.*}_average.txt; done
+cd average
+#join the tables
+join Cereals_average.txt Root_crops_average.txt > pingas
+join pingas Nonpermanent_industrial_crops_average.txt > pino
+join pino Dry_pulses_vegetables_and_flowers_average.txt > pingas
+join pingas Fodder_crops_average.txt > pino
+join pino Permanent_crops_fruit_trees_average.txt > pingas
+join pingas Other_permanent_crops_average.txt > pino
+join pino Broadleaved_woodland_average.txt > pingas
+join pingas Coniferous_woodland_average.txt > pino
+join pino Mixed_woodland_average.txt > pingas
+join pingas Grassland_with_sparse_tree_shrub_cover_average.txt > pino
+join pino Grassland_without_tree_shrub_cover_average.txt > pingas
+join pingas Spontaneously_revegetated_surfaces_average.txt > pino
+join pino Others_average.txt > bingas
+#adding header
+sed '1 i\ARO Name Drug_Class Antibiotic Resistance_Mechanism Cereals Root_crops Nonpermanent_industrial_crops Dry_pulses_vegetables_and_flowers Fodder_crops Permanent_crops_fruit_trees Other_permanent_crops Broadleaved_woodland Coniferous_woodland Mixed_woodland Grassland_with_sparse_tree_shrub_cover Grassland_without_tree_shrub_cover Spontaneously_revegetated_surfaces Others' bingas > average_raw_name_plants.txt
 #ppm_average_terrain_type.txt was exported and converted to ppm_average_terrain_type.xlsx 
-#produce stacked bar plot of drug class in italy grouped by terrain type and plot the Log2FoldChange
+#same with soil type
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Woodland"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Cropland"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Others"
+Rscript /mnt/vol1/projects/LUCAS/functions/select_test.r --token="Grassland"
+for file in $(ls); do awk '{sum = 0; for (i = 2; i <= NF; i++) sum += $i; sum /= (NF-1); print$0 " "  sum}' $file > outfile; awk '{print$1 " " $NF}' outfile |  awk '{if (NR!=1) {print}}' > average/${file%.*}_average.txt; done
+cd average
+join Cropland_average.txt Grassland_average.txt > zino
+join zino Others_average.txt > pingas
+join pingas Woodland_average.txt > bingas
+sed '1 i\ARO Name Drug_Class Antibiotic Resistance_Mechanism Cropland Grassland Others Woodland' bingas > average_drug_raw_soil.txt
+
+#produce stacked bar plot of drug class grouped by terrain type and plot the Log2FoldChange
 Rscript bar_plot.r
